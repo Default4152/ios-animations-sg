391fb52445c5200fe822a96042b4ca2992e91319
Conner Alegre <cga0@pm.me>
2018-08-29 13:44:34 -0600

    add toggle button with ibaction

--- ios-animations-sg/ViewController.swift
+++ ios-animations-sg/ViewController.swift
@@ -20,6 +20,10 @@ class ViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
 
-
+  @IBAction func toggle(_ sender: Any) {
+    shouldScramble = !shouldScramble
+  }
+  
+  var shouldScramble: Bool = false
 }
 
